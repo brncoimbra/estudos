1db60d1b9284c20671b95946b453d9ab1044d406
Bruno Coimbra <57022639+brncoimbra@users.noreply.github.com>
2023-01-05 22:09:13 -0300

    estudos react hooks
    learning useReducer

--- react/hooks/src/views/examples/UseReducer.jsx
+++ react/hooks/src/views/examples/UseReducer.jsx
@@ -1,13 +1,60 @@
-import React from "react";
+import React, { useReducer } from "react";
 import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
+
+const initialState = {
+  cart: [],
+  products: [],
+  user: null,
+  // foco...
+  number: 0,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "add2ToNumber":
+      return { ...state, number: state.number + 2 };
+    case "login":
+      return { ...state, user: { name: action.payload } };
+    default:
+      return state;
+  }
+}
 
 const UseReducer = (props) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <div className="UseReducer">
       <PageTitle
         title="Hook UseReducer"
         subtitle="Uma outra forma de ter estado em componentes funcionais!"
       />
+
+      <SectionTitle title="Exercício #01" />
+      <div className="center">
+        {state.user ? (
+          <span className="te">{state.user.name}</span>
+        ) : (
+          <span className="text">Sem Usuário</span>
+        )}
+
+        <span className="text">{state.number}</span>
+        <div>
+          <button
+            className="btn"
+            onClick={() => dispatch({ type: "login", payload: "Maria" })}
+          >
+            Login
+          </button>
+
+          <button
+            className="btn"
+            onClick={() => dispatch({ type: "add2ToNumber" })}
+          >
+            +2
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
