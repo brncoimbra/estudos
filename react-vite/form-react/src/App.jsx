53887f686e2762e18c2d2ff422ff1ccdb08c6de7
Bruno Coimbra <57022639+brncoimbra@users.noreply.github.com>
2023-01-17 17:16:04 -0300

    react form
    aprofundando em validações simples de um formulario para entrar no conteudo com react hook form

---
+++ react-vite/form-react/src/App.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import './App.css';
+import Form from './components/Form';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Front Beginners</h2>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
