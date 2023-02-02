1c8c8902f9eeddf2ac98cedd609448ca934a0683
Bruno Coimbra <57022639+brncoimbra@users.noreply.github.com>
2023-02-02 16:18:46 -0300

    create card component

--- react-vite/react-redux/src/components/Card.jsx
+++ react-vite/react-redux/src/components/Card.jsx
@@ -1,9 +1,20 @@
-import './Card.css'
+import "./Card.css";
 
-export default props => {
-  return (
-    <div>
-      Card
-    </div>
-  )
+function getColor(props) {
+  if (props.red) return "Red";
+  if (props.green) return "Green";
+  if (props.blue) return "Blue";
+  if (props.purple) return "Purple";
+  return ""
 }
+
+export default (props) => {
+  return (
+    <div className={`Card ${getColor(props)}`}>
+      <div className='Header'>
+        <span className='Title'>{props.title}</span>
+      </div>
+      <div className='Content'>{props.children}</div>
+    </div>
+  );
+};
