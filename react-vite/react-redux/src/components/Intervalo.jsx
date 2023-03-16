f40c7ee257b0202bced29f9d02c83fbdd2e13471
Bruno Coimbra <57022639+brncoimbra@users.noreply.github.com>
2023-03-16 16:55:02 -0300

    action 3

--- react-vite/react-redux/src/components/Intervalo.jsx
+++ react-vite/react-redux/src/components/Intervalo.jsx
@@ -1,7 +1,10 @@
 import "./Intervalo.css";
 import Card from "./Card";
 import { connect } from "react-redux";
-import { alterarNumeroMinimo } from "../store/actions/numeros";
+import {
+  alterarNumeroMinimo,
+  alterarNumeroMaximo,
+} from "../store/actions/numeros";
 
 function Intervalo(props) {
   const { min, max } = props;
@@ -11,12 +14,20 @@ function Intervalo(props) {
       <div className='Intervalo'>
         <span>
           <strong>Mínimo:</strong>
-          <input type='number' value={min} onChange={e => props.alterarMinimo(+e.target.value)} />
+          <input
+            type='number'
+            value={min}
+            onChange={(e) => props.alterarMinimo(+e.target.value)}
+          />
         </span>
 
         <span>
           <strong>Máximo:</strong>
-          <input type='number' value={max} onChange={e => props.alterarMinimo(+e.target.value)} />
+          <input
+            type='number'
+            value={max}
+            onChange={(e) => props.alterarMaximo(+e.target.value)}
+          />
         </span>
       </div>
     </Card>
@@ -34,6 +45,10 @@ function mapDispatchToProps(dispatch) {
   return {
     alterarMinimo(novoNumero) {
       const action = alterarNumeroMinimo(novoNumero);
+      dispatch(action);
+    },
+    alterarMaximo(novoNumero) {
+      const action = alterarNumeroMaximo(novoNumero);
       dispatch(action);
     },
   };
