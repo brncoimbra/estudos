aad84c6b27cedc81b37b31574d1ea2932ed7d515
Bruno Coimbra <57022639+brncoimbra@users.noreply.github.com>
2023-03-19 18:58:39 -0300

    learning data types on angular

--- angular/conceitos/src/app/components/first-component/first-component.component.ts
+++ angular/conceitos/src/app/components/first-component/first-component.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
-  styleUrls: ['./first-component.component.css']
+  styleUrls: ['./first-component.component.css'],
 })
 export class FirstComponentComponent {
-
+  name: string = 'Bruno';
+  age: number = 30;
+  job = 'Programador';
+  hobbies = ['Jogar', 'Estudar', 'Tocar instrumentos']
+  car = {
+    name: 'Polo',
+    year: 2019
+  }
 }
