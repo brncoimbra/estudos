9da2257a44e7e0b62a24eede961795493a759adc
Bruno Coimbra <57022639+brncoimbra@users.noreply.github.com>
2023-03-23 16:54:28 -0300

    interface on angular

--- angular/conceitos/src/app/components/list-render/list-render.component.ts
+++ angular/conceitos/src/app/components/list-render/list-render.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Animal } from 'src/app/Animal';
 
 @Component({
   selector: 'app-list-render',
@@ -6,22 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-render.component.css'],
 })
 export class ListRenderComponent {
-  animals = [
+  animals: Animal[] = [
     {
       name: 'Turca',
       type: 'Dog',
+      age: 4,
     },
     {
       name: 'Tom',
       type: 'Cat',
+      age: 10,
     },
     {
       name: 'Frida',
       type: 'Dog',
+      age: 5,
     },
     {
       name: 'Bob',
       type: 'Horse',
+      age: 1,
     },
   ];
+
+  animalDetails = '';
+
+  showAge(animal: Animal): void {
+    this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos!`;
+  }
 }
