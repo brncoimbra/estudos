5a3accd088fa83dca53295d4d190e927481dcce7
Bruno Coimbra <57022639+brncoimbra@users.noreply.github.com>
2023-03-26 16:46:28 -0300

    route angular

--- angular/conceitos/src/app/app-routing.module.ts
+++ angular/conceitos/src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChangeNumberComponent } from './components/change-number/change-number.component';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
+import { DirectivesComponent } from './components/directives/directives.component';
+import { EmitterComponent } from './components/emitter/emitter.component';
+import { EventosComponent } from './components/eventos/eventos.component';
+import { IfRenderComponent } from './components/if-render/if-render.component';
+import { ParentDataComponent } from './components/parent-data/parent-data.component';
+import { PipesComponent } from './components/pipes/pipes.component';
+import { TwoWayBindingComponent } from './components/two-way-binding/two-way-binding.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FirstComponentComponent },
+  { path: 'list', component: ListRenderComponent },
+  { path: 'directives', component: DirectivesComponent },
+  { path: 'emitter', component: EmitterComponent },
+  { path: 'eventos', component: EventosComponent },
+  { path: 'ifrender', component: IfRenderComponent },
+  { path: 'parentdata', component: ParentDataComponent },
+  { path: 'pipes', component: PipesComponent },
+  { path: 'twoway', component: TwoWayBindingComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
